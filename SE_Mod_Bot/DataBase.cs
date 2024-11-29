b8be290cc622c24d96da6d88c4839265e309c22e
Marco-Zechner <marcoflorian00956@gmail.com>
2024-11-29 01:57:57 +0100

    Improved File reading and writing. Now using config and data folder

--- SE_Mod_Bot/DataBase.cs
+++ SE_Mod_Bot/DataBase.cs
@@ -2,25 +2,56 @@
     public class DataBase {
 
         public static async Task<string> ReadFile(string fileName) {
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+            string dataDir = Environment.GetEnvironmentVariable("DATA_DIR") ?? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data");
+            string filePath = Path.Combine(dataDir, fileName);
 
             if (!File.Exists(filePath)) {
-                File.WriteAllText(filePath, "");
-                Console.WriteLine($"File '{fileName}' created at {filePath}");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{DateTime.Now,-19} File '{fileName}' at {filePath} not found.");
+                Console.ResetColor();
+                return "";
             }
 
             return await File.ReadAllTextAsync(filePath);
         }        
         
         public static async Task WriteFile(string fileName, string content) {
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+            string dataDir = Environment.GetEnvironmentVariable("DATA_DIR") ?? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data");
+            string filePath = Path.Combine(dataDir, fileName);
 
             if (!File.Exists(filePath)) {
                 File.WriteAllText(filePath, "");
-                Console.WriteLine($"File '{fileName}' created at {filePath}");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"{DateTime.Now,-19} File '{fileName}' created at {filePath}");
+                Console.ResetColor();
             }
 
             await File.WriteAllTextAsync(filePath, content);
+        }
+
+        private static async Task<string> ReadFromConfig(string fileName) {
+            string configDir = Environment.GetEnvironmentVariable("CONFIG_DIR") ?? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config");
+
+            string filePath = Path.Combine(configDir, fileName);
+
+            if (!File.Exists(filePath)) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{DateTime.Now,-19} File '{fileName}' at {filePath} not found.");
+                Console.ResetColor();
+                return "";
+            }
+
+            return await File.ReadAllTextAsync(filePath);
+        }
+
+        public static async Task LoadEnv(string fileName) {
+            var lines = (await ReadFromConfig(fileName)).Split('\n');
+            foreach (var line in lines) {
+                var parts = line.Split('=', 2);
+                if (parts.Length == 2) {
+                    Environment.SetEnvironmentVariable(parts[0], parts[1]);
+                }
+            }
         }
     }
 }
--- SE_Mod_Bot/Program.cs
+++ SE_Mod_Bot/Program.cs
@@ -6,6 +6,8 @@ namespace SE_Mod_Bot {
     public class Program {
         // Program entry point
         static async Task Main(string[] args) {
+            Console.WriteLine("Start with args: " + string.Join(", ", args));
+            
             ProgramGlobal.client = new DiscordSocketClient(new DiscordSocketConfig {
                 LogLevel = LogSeverity.Info,
             });
@@ -19,7 +21,7 @@ namespace SE_Mod_Bot {
 
         private static async Task MainAsync() {
 
-            LoadEnv();
+            await DataBase.LoadEnv("token.env");
 
             // Login and connect.
             await ProgramGlobal.client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("Discord_SE_Bot_Token"));
@@ -27,16 +29,6 @@ namespace SE_Mod_Bot {
 
             // Wait infinitely so your bot actually stays connected.
             await Task.Delay(Timeout.Infinite);
-        }
-        
-        private static void LoadEnv() {
-            var lines = File.ReadAllLines("D:\\UNI\\VisualStudio\\SE_Mod_Bot\\SE_Mod_Bot\\token.env");
-            foreach (var line in lines) {
-                var parts = line.Split('=', 2);
-                if (parts.Length == 2) {
-                    Environment.SetEnvironmentVariable(parts[0], parts[1]);
-                }
-            }
         }
     }
 }
